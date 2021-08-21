e2fe21c4b538c350c9127a2596e1d0ef6c8c6840
liqiongyu <liqiongyuid@qq.com>
2021-08-21 08:53:27 +0800

    点击按钮新增了资源储备的智能判断

--- main.py
+++ main.py
@@ -18,17 +18,48 @@ class ADarkRoom:
         self.click_ele(By.CSS_SELECTOR, ".hyper")  # 加速
         self.click_ele(By.CSS_SELECTOR, ".hyper")  # 二次点击 一次点击会失效 可能是由于需要先聚焦
 
-    def click_button_id(self, ele_id):
-        """
-        点击id属性的按钮
-        :param ele_id: 按钮的id名称
-        """
-        WebDriverWait(self.driver, 70).until(ec.visibility_of_element_located((By.ID, ele_id))).click()
-
-    def click_button(self, ele_id):
+    def click_button(self, button_id):
         """
         点击按钮，用id定位
-        :param ele_id: 按钮的id名称
+        :param button_id: 按钮的id名称
+        """
+        try:
+            print(button_id)  # 打印id方便观察程序运行到哪一步
+            self.handling_events()  # 处理各种事件
+            self.click_page(button_id)  # 点击到对应页面
+            if self.is_exist(By.CSS_SELECTOR, "#{0} > .tooltip".format(button_id)):  # 判断该按钮是否需要材料
+                resource_id = self.not_enough(button_id)  # 获取那些材料不够
+                if resource_id is None:   # 判断材料是否足够
+                    self.click_button_id(button_id)  # 点击按钮
+            else:
+                self.click_button_id(button_id)  # 点击按钮
+        except ElementClickInterceptedException:  # 防止点击过程中出现新事件
+            self.handling_events()
+            self.click_button_id(button_id)
+
+    def click_button_id(self, button_id):
+        """
+        点击id属性的按钮
+        :param button_id: 按钮的id名称
+        """
+        WebDriverWait(self.driver, 45).until(lambda x: self.is_clicked(x, button_id))  # 开了加速最多等待45秒，陷阱原速90秒
+        self.click_ele(By.ID, button_id)
+
+    def click_ele(self, by, value):
+        """
+        移动到对应元素并点击
+        :param by: By.ID By.CLASS_NAME
+        :param value: 对应值
+        """
+        self.driver.execute_script("arguments[0].click();", self.driver.find_element(by, value))
+
+    @staticmethod
+    def is_clicked(driver, button_id):
+        """
+        判断按钮是否可点击
+        :param driver: 浏览器实例
+        :param button_id: 按钮id
+        :return: 按钮可点击就返回按钮，不能就返回False
         """
         try:
             title = self.get_event_title()
